a98dc6eab54221b12e619e672ffbf3c6b90c5b65
Justyna Spital <justynaspital@gmail.com>
2024-03-14 18:45:16 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+
+            Console.WriteLine("How many money you would like to conver?");
+            string money = Console.ReadLine();
+            double moneys = Convert.ToDouble(money);
+
+            Console.WriteLine($"{moneys} is equal to...");
+
+            double gold = 10;
+            double silver = 5;
+
+            double goldCoins = Math.Floor(moneys / gold);
+            double restOfMoney = moneys % gold;
+            double silverCoins = Math.Floor(restOfMoney / silver);
+            double restOfMoneytwo = restOfMoney % silver;
+
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins: {restOfMoneytwo}");
+
+        }
+    }
+}
